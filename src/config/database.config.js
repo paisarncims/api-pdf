7dde9824cc5840b9ab3c180d329f8b8b3dbe5d3d
Mos Zenith <wongsakorn.m@zenithme.co.th>
2022-08-25 21:20:48 +0700

    Update

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,39 +1,40 @@
 const configMongodb = () => {
     if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === undefined) {
         dbConfigMongo = {
-            username: process.env.IGC_MONGODB_Username,
-            password: process.env.IGC_MONGODB_Password,
-            host: process.env.IGC_MONGODB_Host,
-            dbname: process.env.IGC_MONGODB_DatabaseName,
-            port: process.env.IGC_MONGODB_Port,
+            username: process.env.MONGODB_Username,
+            password: process.env.MONGODB_Password,
+            host: process.env.MONGODB_Host,
+            dbname: process.env.MONGODB_DatabaseName,
+            port: process.env.MONGODB_Port,
         }
-        MongoClient_url = `mongodb://${dbConfigMongo.username}:${dbConfigMongo.password}@${dbConfigMongo.host}:${dbConfigMongo.port}/${dbConfigMongo.dbname}`
+        MongoClient_url = `mongodb+srv://${dbConfigMongo.username}:${dbConfigMongo.password}@${dbConfigMongo.host}/${dbConfigMongo.dbname}?authSource=admin&replicaSet=cims-mongodb&tls=true`
         dbConfigMongo.MongoClient_url = MongoClient_url;
-        
         return dbConfigMongo;
+
     } else if (process.env.NODE_ENV === 'production') {
         dbConfigMongo = {
-            username: process.env.IGC_MONGODB_Username,
-            password: process.env.IGC_MONGODB_Password,
-            host: process.env.IGC_MONGODB_Host,
-            dbname: process.env.IGC_MONGODB_DatabaseName,
-            port: process.env.IGC_MONGODB_Port,
+            username: process.env.MONGODB_Username,
+            password: process.env.MONGODB_Password,
+            host: process.env.MONGODB_Host,
+            dbname: process.env.MONGODB_DatabaseName,
+            port: process.env.MONGODB_Port,
         }
-        MongoClient_url = `mongodb://${dbConfigMongo.username}:${dbConfigMongo.password}@${dbConfigMongo.host}:${dbConfigMongo.port}/${dbConfigMongo.dbname}`
+        MongoClient_url = `mongodb+srv://${dbConfigMongo.username}:${dbConfigMongo.password}@${dbConfigMongo.host}/${dbConfigMongo.dbname}?authSource=admin&replicaSet=cims-mongodb&tls=true`
         dbConfigMongo.MongoClient_url = MongoClient_url
 
         return dbConfigMongo;
     } else {
-        dbConfigMongo = {
-            username: process.env.IGC_MONGODB_Username,
-            password: process.env.IGC_MONGODB_Password,
-            host: process.env.IGC_MONGODB_Host,
-            dbname: process.env.IGC_MONGODB_DatabaseName,
-            port: process.env.IGC_MONGODB_Port,
-        }
-        MongoClient_url = `mongodb://${dbConfigMongo.username}:${dbConfigMongo.password}@${dbConfigMongo.host}:${dbConfigMongo.port}/${dbConfigMongo.dbname}`
-        dbConfigMongo.MongoClient_url = MongoClient_url
 
+        dbConfigMongo = {
+            username: process.env.MONGODB_Username,
+            password: process.env.MONGODB_Password,
+            host: process.env.MONGODB_Host,
+            dbname: process.env.MONGODB_DatabaseName,
+            port: process.env.MONGODB_Port,
+        }
+        MongoClient_url = `mongodb+srv://${dbConfigMongo.username}:${dbConfigMongo.password}@${dbConfigMongo.host}/${dbConfigMongo.dbname}?authSource=admin&replicaSet=cims-mongodb&tls=true`
+        dbConfigMongo.MongoClient_url = MongoClient_url
+        
         return dbConfigMongo
     }
 }
